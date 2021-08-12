3de7b05ef5c4cf7bb17ecf1f23ac99a8a752f166
Bomb Atomically <ieugenelee@gmail.com>
2021-08-12 10:41:13 -0700

    created testing functions

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,18 +1,34 @@
 const { expect } = require('@jest/globals')
 const Employee = require('../lib/Employee')
-test ('checkingEmployee', function(){
+test('checkingEmployee', function () {
     const myEmployee = new Employee()
-    expect(typeof(myEmployee).toBe('object'))
+    expect(typeof (myEmployee).toBe('object'))
 })
-test ('checkingEmployeeName', function(){
+test('checkingEmployeeName', function () {
     const myEmployee = new Employee('Eugene')
     expect((myEmployee.name).toBe('Eugene'))
 })
-test ('checkingEmployeeId', function(){
+test('checkingEmployeeId', function () {
     const myEmployee = new Employee('Eugene', 100)
     expect((myEmployee.id).toBe(100))
 })
-test ('checkingEmployeeEmail', function(){
-    const myEmployee = new Employee("Eugene",100,'Eugene@gmail.com')
+test('checkingEmployeeEmail', function () {
+    const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
     expect((myEmployee.email).toBe('Eugene@gmail.com'))
+})
+test('checkingEmployeeEmail', function () {
+    const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
+    expect((myEmployee.getName()).toBe('Eugene'))
+})
+test('checkingEmployeeRole', function () {
+    const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
+    expect((myEmployee.getId()).toBe(100))
+})
+test('checkingEmployeeRole', function () {
+    const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
+    expect((myEmployee.getEmail()).toBe('Eugene@gmail.com'))
+})
+test('checkingEmployeeRole', function () {
+    const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
+    expect((myEmployee.getRole()).toBe('Employee'))
 })
