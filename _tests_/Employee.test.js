a65b187c4c9ae124189aa6c717b69659224e9c81
Bomb Atomically <ieugenelee@gmail.com>
2021-08-12 11:00:38 -0700

    finished employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,33 +2,33 @@ const { expect } = require('@jest/globals')
 const Employee = require('../lib/Employee')
 test('checkingEmployee', function () {
     const myEmployee = new Employee()
-    expect(typeof (myEmployee).toBe('object'))
+    expect((typeof(myEmployee))).toBe('object') // error
 })
 test('checkingEmployeeName', function () {
     const myEmployee = new Employee('Eugene')
-    expect((myEmployee.name).toBe('Eugene'))
+    expect((myEmployee.name)).toBe('Eugene')
 })
 test('checkingEmployeeId', function () {
     const myEmployee = new Employee('Eugene', 100)
-    expect((myEmployee.id).toBe(100))
+    expect((myEmployee.id)).toBe(100)
 })
 test('checkingEmployeeEmail', function () {
     const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
-    expect((myEmployee.email).toBe('Eugene@gmail.com'))
+    expect((myEmployee.email)).toBe('Eugene@gmail.com')
 })
-test('checkingEmployeeEmail', function () {
+test('checkingEmployeeNameFunction', function () {
     const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
-    expect((myEmployee.getName()).toBe('Eugene'))
+    expect((myEmployee.getName())).toBe('Eugene')
 })
-test('checkingEmployeeRole', function () {
+test('checkingEmployeeIdFunction', function () {
     const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
-    expect((myEmployee.getId()).toBe(100))
+    expect((myEmployee.getId())).toBe(100)
 })
-test('checkingEmployeeRole', function () {
+test('checkingEmployeeEmailFunction', function () {
     const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
-    expect((myEmployee.getEmail()).toBe('Eugene@gmail.com'))
+    expect((myEmployee.getEmail())).toBe('Eugene@gmail.com')
 })
-test('checkingEmployeeRole', function () {
+test('checkingEmployeeRoleFunction', function () {
     const myEmployee = new Employee("Eugene", 100, 'Eugene@gmail.com')
-    expect((myEmployee.getRole()).toBe('Employee'))
+    expect((myEmployee.getRole())).toBe('Employee')
 })
